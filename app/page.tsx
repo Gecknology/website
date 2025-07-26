01708b17a754d8e5d0c5fbc532e9da0f5e4b3162
Sean <sean.l.tosello@gmail.com>
2025-07-26 00:23:08 -0700

    removed import reference to removed component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Hero from "@/components/hero"
 import Features from "@/components/features"
-import CTA from "@/components/cta"
 import { Separator } from "@/components/ui/separator"
 
 export default function Home() {
